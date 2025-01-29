6359125a6f8bfda353ae949d5011989820a26f4c
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2025-01-29 18:39:28 +0200

    new

--- Chat App/client/src/Chat.jsx
+++ Chat App/client/src/Chat.jsx
@@ -1,9 +1,82 @@
 import styles from './styles/mainStyles.module.css'
+import {useEffect, useState} from "react";
+import {io} from "socket.io-client";
+const socket = io.connect("http://localhost:8000");
 
 const Chat = () => {
+    const [isChatOpen, setIsChatOpen] = useState(false);
+    const [userName, setUserName] = useState('')
+    const [myMessage, setMyMessage] = useState('')
+    const [messagesReceived, setMessagesReceived] = useState([])
+
+    useEffect(() => {
+        socket.on("receiveMessage", (data) => {
+            setMessagesReceived((prevMessages) => [...prevMessages, data]);
+            console.log(data)
+        })
+
+        // Cleanup to avoid duplicate listeners
+        return () => {
+            socket.off("receiveMessage");
+        };
+    }, [])
+
+    const handleStartChat = () => {
+        if (userName.trim().length > 0) {
+            setIsChatOpen(true);
+        }
+    }
+
+    const sendMessage = () => {
+        if (myMessage.trim().length > 0) {
+            socket.emit("sendMessage", { user: userName, message: myMessage });
+            setMessagesReceived([...messagesReceived, {user: userName, message:myMessage}]);
+            setMyMessage('');
+        }
+        console.log(myMessage);
+        console.log(messagesReceived);
+    };
+
     return (
         <div className={styles.chat}>
-            <h1>Hello World!</h1>
+            <p>username: {userName}</p>
+            { !isChatOpen ? (
+                // Before start chat
+                <div>
+                    <h3>Get Started right now!</h3>
+                    <br/>
+                    <div>
+                        <div className="mb-3">
+                            <label htmlFor="userName" className="form-label">I want to start chatting with the name:</label>
+                            <input type="text"
+                                   className="form-control"
+                                   id="userName"
+                                   onChange={e => setUserName(e.target.value)}
+                                   value={userName}
+                                   placeholder={"Your name..."} />
+                        </div>
+                        <button type="submit" className="btn btn-success" onClick={handleStartChat}>Start Chatting</button>
+                    </div>
+                </div>
+            ) : (
+                // After start Chat
+                <div>
+                    <div className={styles.chatMessagesBox}>
+                        {messagesReceived.map((message, index) => (
+                            <p key={index}><strong>{message.user}: </strong>{message.message}</p>
+                        ))}
+                    </div>
+                    <div className={styles.chatInput}>
+                        <input type="text"
+                               className="form-control"
+                               id="myMessage"
+                               onChange={e => setMyMessage(e.target.value)}
+                               value={myMessage}
+                               placeholder={"..."} />
+                        <button className="btn btn-primary" onClick={sendMessage}>Send</button>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
--- Chat App/server/server.js
+++ Chat App/server/server.js
@@ -1,20 +1,35 @@
 require('dotenv').config();
-    require('./config/mongoose.config');
+require('./config/mongoose.config');
 
-    const express = require('express');
-    const app = express();
-    const port = process.env.PORT;
-    const cors = require('cors');
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+const cors = require('cors');
+const http = require("http");
+const { Server } = require("socket.io");
 
-    // Middleware
-    app.use(cors());
-    app.use(express.json(), express.urlencoded({ extended: true }));
+// Middleware
+app.use(cors());
+app.use(express.json(), express.urlencoded({ extended: true }));
 
-    // Routes
-    // const authorRoutes = require("./routes/author.routes");
-    // authorRoutes(app);
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+});
 
-    // Start server
-    app.listen(port, () => {
-        console.log(`Listening at Port ${port}`);
-    });
+// Socket.io connection handler
+io.on("connection", (socket) => {
+    console.log("User connected, socket id=" + socket.id);
+    socket.on("sendMessage", (data) => {
+        console.log(data);
+        socket.broadcast.emit("receiveMessage", data);
+    })
+})
+
+// Start server
+server.listen(port, () => {
+    console.log(`Listening at Port ${port}`);
+});
